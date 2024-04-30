1e8a026709a3a415391984ef7406f3efbe49c3bc
Surya-0804 <159000250+Surya-0804@users.noreply.github.com>
2024-04-30 11:32:29 +0530

    Create Area of a Rectangle.java

---
+++ Math/Easy/Area of a Rectangle.java
@@ -0,0 +1,11 @@
+import java.util.* ;
+import java.io.*; 
+public class Rectangle { 
+    // Write your code here.
+    int length;
+    int breadth;
+
+    int getArea(){
+        return length*breadth;
+    }
+}
