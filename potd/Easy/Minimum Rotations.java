eda7fd41a4ae084ccc9a2b947d7a017571a1faf7
Surya-0804 <159000250+Surya-0804@users.noreply.github.com>
2024-05-05 10:21:53 +0530

    Update Minimum Rotations.java

--- potd/Easy/Minimum Rotations.java
+++ potd/Easy/Minimum Rotations.java
@@ -1,19 +1,14 @@
-//10/11 passed
 import java.util.* ;
 import java.io.*; 
 public class Solution {
 
 	public static int minimumRotations(int n, String s) {
 		// Write your code here.
-		String temp=s;
-		int min=Integer.MAX_VALUE;
-		for(int i=0;i<n;i++){
-			temp=temp.substring(n-1)+temp.substring(0,n-1);
-			if(temp.equals(s)){
-				min=Math.min(min,i+1);
-			}
+		for(int i=1;i<n;i++){
+			if(s.charAt(i)!=s.charAt(i-1))
+				return s.length();
 		}
-		return min;
+		return 1;
 	}
 
 }
