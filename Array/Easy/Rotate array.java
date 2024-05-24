ae70f59177566e9c64771416959dc7b61c0fea98
Ram Sai Sri Surya Abothula <159000250+Surya-0804@users.noreply.github.com>
2024-05-24 14:51:41 +0530

    Update Rotate array.java

--- Array/Easy/Rotate array.java
+++ Array/Easy/Rotate array.java
@@ -3,14 +3,24 @@ import java.util.ArrayList;
 public class Solution {
 	public static ArrayList<Integer> rotateArray(ArrayList<Integer> arr, int k) {
         // Write your code here.
-        ArrayList<Integer> answer=new ArrayList<>();
-        for(int i=k ; i<arr.size() ; i++){
-            answer.add(arr.get(i));
+        k=k%arr.size();
+       
+        reverse(arr, 0, k-1);
+
+        reverse(arr, k, arr.size()-1);
+
+        reverse(arr, 0, arr.size()-1);
+
+        return arr;
+    }
+
+    private static void reverse(ArrayList<Integer> arr, int start , int end){
+        while(start <end){
+            int temp=arr.get(start);
+            arr.set(start, arr.get(end));
+            arr.set(end,temp);
+            start++;
+            end--;
         }
-        for(int i=0; i<k; i++)
-            answer.add(arr.get(i));
-        
-        return answer;
-        
     }
 }
