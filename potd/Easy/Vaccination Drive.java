839a459860c04a2bfd264a51f5928d1f56efe7ea
Ram Sai Sri Surya Abothula <159000250+Surya-0804@users.noreply.github.com>
2024-06-11 17:11:11 +0530

    Create Vaccination Drive.java

---
+++ potd/Easy/Vaccination Drive.java
@@ -0,0 +1,11 @@
+import java.util.* ;
+import java.io.*; 
+
+public class Solution {
+
+	public static int getMaximumHouses(int k, int l) {
+		// Write your code here.
+		return (int)Math.pow(2,l)-k;
+	}
+
+}
