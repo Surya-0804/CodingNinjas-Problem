a1a0e972ed40f6869093be84fd50fdb19d7ae9ef
Ram Sai Sri Surya Abothula <159000250+Surya-0804@users.noreply.github.com>
2024-06-12 18:03:58 +0530

    Create Print Name and Age.java

---
+++ Java/Easy/Print Name and Age.java
@@ -0,0 +1,15 @@
+import java.util.* ;
+import java.io.*; 
+import java.util.Scanner;
+
+class Solution {
+	
+	public static void main(String args[]) {
+		
+		// Write your code here
+		Scanner sc=new Scanner(System.in);
+		String name=sc.next();
+		int age=sc.nextInt();
+		System.out.print("My name is "+name+" and my age is "+age+".");		
+	}
+}
