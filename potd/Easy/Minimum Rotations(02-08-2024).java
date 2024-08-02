b64623e7e99d93c41437f1657340fbd740bb67e7
Ram Sai Sri Surya Abothula <159000250+Surya-0804@users.noreply.github.com>
2024-08-02 20:30:20 +0530

    Create Minimum Rotations(02-08-2024).java

---
+++ potd/Easy/Minimum Rotations(02-08-2024).java
@@ -0,0 +1,15 @@
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+
+	public static int minimumRotations(int n, String s) {
+		// Write your code here.
+		for(int i=1; i<n; i++){
+			if(s.charAt(i)!=s.charAt(i-1)){
+				return n;
+			}
+		}
+		return 1;
+	}
+
+}
